0508d5fbf7127fdb2c2e77bea2e0b4e1fa675a1d
moichi <zzikbu@gmail.com>
2024-06-17 23:55:45 +0900

    Feat: Accountsummary 네트워킹 구현

---
+++ Bankey/Bankey/Utils/Data+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Data+Utils.swift
+//  Bankey
+//
+//  Created by 이승민 on 6/17/24.
+//
+
+import UIKit
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
