31423c26eb1f7627a94ded91c92b3204a095a431
moichi <zzikbu@gmail.com>
2024-06-18 14:06:55 +0900

    Feat: Json 파싱 단위 테스트 구현

---
+++ Bankey/BankeyUnitTests/AccountSummary/ProfileTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileTests.swift
+//  BankeyUnitTests
+//
+//  Created by 이승민 on 6/18/24.
+//
+
+import XCTest
+
+@testable import Bankey
+
+class ProfileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testCanParse() throws {
+        let json = """
+        {
+        "id": "1",
+        "first_name": "Kevin",
+        "last_name": "Flynn",
+        }
+        """ // 백엔드에서 주는 값이 변경되면 수정
+        
+        let data = json.data(using: .utf8)!
+        let result = try! JSONDecoder().decode(Profile.self, from: data)
+        
+        XCTAssertEqual(result.id, "1")
+        XCTAssertEqual(result.firstName, "Kevin")
+        XCTAssertEqual(result.lastName, "Flynn")
+    }
+}
